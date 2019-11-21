f43ed796900f35f3873f8dd8e756bcb5c13040f8
cabal82 <fabriziogallici@gmail.com>
2019-11-21 15:16:53 +0100

    clean code

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-
+/* Creare un calendario dinamico con le festività.Partiamo dal gennaio 2018 dando la possibilità di cambiare mese, 
+gestendo il caso in cui l’API non possa ritornare festività.
+Il calendario partirà da gennaio 2018 e si concluderà a dicembre 2018(unici dati disponibili sull’API). */
 
 function getMonth(monthIndex) {
   const year = 2018;
@@ -9,7 +11,7 @@ function getMonth(monthIndex) {
     success: function (data) {
       var objMonth = data.response;
       console.log('success', objMonth);
-      convertMonth(monthIndex, objMonth, year);
+      convertMonth(monthIndex, year, objMonth);
     },
     error: function (error) {
       console.log("error", error);
@@ -19,7 +21,7 @@ function getMonth(monthIndex) {
 
 //estrapolazione dati num giorni in un mese, nome mesi, anno e 
 //divisione step successivi dati mese e controllo festività
-function convertMonth(monthIndex, objMonth, yearNum) {
+function convertMonth(monthIndex, yearNum, objMonth) {
   var monthNum = monthIndex + 1; // aumento per parificare notazione momentjs
   // var firstDayInMonthDate = objMonth[0].date;
   var currentMonth = yearNum + '-' + monthNum;
@@ -28,21 +30,21 @@ function convertMonth(monthIndex, objMonth, yearNum) {
   // console.log('daysInAMonth', daysInAMonth, 'monthname', monthName, 'year', yearNum);
   //ripulisco il container prima di immettere nuovi dati
   $('.days-container').empty();
-  evaluateMonthData(daysInAMonth, monthName, yearNum);
+  evaluateMonthData(daysInAMonth, monthName, yearNum, objMonth);
   //stampa titolo con mese corrente
-  document.getElementById('current-month').innerText = monthName + ' - ' + yearNum;
-  //inserire le festività presenti nel calendario
-  checkFestivity(objMonth);
+  document.getElementById('current-month').innerText = monthName + ' - ' + yearNum; 
 }
 
 //ciclo in base ai numeri del mese, estrapolando numero del giorno e giorno della settimana
-function evaluateMonthData(daysInAMonth, monthName, yearNum) {
+function evaluateMonthData(daysInAMonth, monthName, yearNum, objMonth) {
   for (i = 1; i <= daysInAMonth; i++) {
     var currentDate = moment(yearNum + '-' + monthName + '-' + i, 'YYYY-MMMM-D').format('YYYY-MM-DD');
     var dayOfTheWeek = moment(currentDate, "YYYY-MM-DD").format('ddd');
     // console.log('dayoftheweek', dayOfTheWeek, 'currendata', currentDate);
     printCalendar(i, dayOfTheWeek, currentDate);
   }
+  //inserire le festività presenti nel calendario
+  checkFestivity(objMonth);
 }
 
 //stampa calendario a schermo con handlebars
@@ -96,7 +98,7 @@ function switchMonthBtns(direction) {
 
 $(document).ready(function () {
   var monthIndex = 0;
-  //inizializzo al primo mese 
+  //inizializzo all'index primo mese 
   getMonth(monthIndex);
 
   var prevMonth = switchMonthBtns("prev");
