15952125a81b872873b3d9b6ce9d6d1991ff5362
cabal82 <fabriziogallici@gmail.com>
2019-11-21 08:04:40 +0100

    day

--- main.js
+++ main.js
@@ -18,14 +18,16 @@ function getMonth() {
 function convertMonth(objMonth) {
   //estrapolare quanti giorni ha un mese
   // var yearAndMonth = objMonth[0].date.slice(0,7);
-  var yearAndMonth = objMonth[0].date;
-  var daysInAMonth = moment(yearAndMonth, "YYYY-MM-DD").daysInMonth();
-  var monthName = moment(yearAndMonth, "YYYY-MM-DD").format('MMMM');
-  var yearNum = moment(yearAndMonth, "YYYY-MM-DD").format('YYYY');
-  console.log(yearAndMonth);
+  var firstDayInMonthDate = objMonth[0].date;
+  var daysInAMonth = moment(firstDayInMonthDate, "YYYY-MM-DD").daysInMonth();
+  var monthName = moment(firstDayInMonthDate, "YYYY-MM-DD").format('MMMM');
+  var yearNum = moment(firstDayInMonthDate, "YYYY-MM-DD").format('YYYY');
+  var dayOfTheWeek = moment(firstDayInMonthDate, "YYYY-MM-DD").format('ddd');
+  console.log(firstDayInMonthDate);
   console.log('daysInAMonth', daysInAMonth);
   console.log('monthname', monthName);
   console.log('year', yearNum);
+  console.log('dayoftheweek', dayOfTheWeek);
   for (i = 1; i <= daysInAMonth; i++) {
     //creare una data del giorno es 2018-01-01 da usare come attributo
     var currentDate = moment(yearNum + '-' + monthName + '-' + i, 'YYYY-MMMM-D').format('YYYY-MM-DD');
@@ -43,17 +45,6 @@ function evaluateMonthData() {
 
 }
 
-function checkFestivity(objMonth) {
-  objMonth.forEach(el => {
-    var festName = el.name;
-    var date = el.date;
-    console.log ('festname', festName, date);
-    //aggiungo festività in pagina e cambio background cella
-    $(`.date[data-name="${date}"]`).html(festName).closest('.days').addClass('bg-fest');
-    
-  });
-}
-
 function printCalendar(day, monthName, currentDate) {
   var source = document.getElementById('calendar-template').innerHTML;
   var calendarTemplate = Handlebars.compile(source);
@@ -63,6 +54,19 @@ function printCalendar(day, monthName, currentDate) {
   $('.container.days-container').append(htmlcalendarData);
 }
 
+function checkFestivity(objMonth) {
+  objMonth.forEach(el => {
+    var festName = el.name;
+    var date = el.date;
+    console.log('festname', festName, date);
+    //aggiungo festività in pagina e cambio background cella
+    $(`.date[data-name="${date}"]`)
+      .html(festName)
+      .closest('.days')
+      .addClass('bg-fest');
+
+  });
+}
 
 $(document).ready(function () {
   getMonth();
