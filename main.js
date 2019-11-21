7e8126aac65860301ed983e3b33a8362d237e3a0
cabal82 <fabriziogallici@gmail.com>
2019-11-21 10:59:14 +0100

    pulizia cod

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function getMonth() {
   $.ajax({
     url: "https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0",
     method: "GET",
-    success: function (data) {    
+    success: function (data) {
       var objMonth = data.response;
       console.log('success', objMonth);
       convertMonth(objMonth);
@@ -14,65 +14,50 @@ function getMonth() {
 }
 
 function convertMonth(objMonth) {
-  //estrapolare quanti giorni ha un mese
-  // var yearAndMonth = objMonth[0].date.slice(0,7);
   var firstDayInMonthDate = objMonth[0].date;
   var daysInAMonth = moment(firstDayInMonthDate, "YYYY-MM-DD").daysInMonth();
   var monthName = moment(firstDayInMonthDate, "YYYY-MM-DD").format('MMMM');
   var yearNum = moment(firstDayInMonthDate, "YYYY-MM-DD").format('YYYY');
-  // var dayOfTheWeek = moment(firstDayInMonthDate, "YYYY-MM-DD").format('ddd');
-  // console.log('dayoftheweek', dayOfTheWeek);
-  console.log(firstDayInMonthDate);
-  console.log('daysInAMonth', daysInAMonth);
-  console.log('monthname', monthName);
-  console.log('year', yearNum);
-  
+  // console.log('daysInAMonth', daysInAMonth);
+  // console.log('monthname', monthName);
+  // console.log('year', yearNum);
 
   evaluateMonthData(daysInAMonth, monthName, yearNum);
   //stampa titolo con mese corrente
   document.getElementById('current-month').innerText = monthName + ' - ' + yearNum;
-  //inserire le festività nella lista
+  //inserire le festività presenti nel calendario
   checkFestivity(objMonth);
 }
 
+//ciclo in base ai numeri del mese, estrapolando numero del giorno e giorno della settimana
 function evaluateMonthData(daysInAMonth, monthName, yearNum) {
   //da pensare come dividere func
   for (i = 1; i <= daysInAMonth; i++) {
     //creare una data del giorno es 2018-01-01 da usare come attributo
     var currentDate = moment(yearNum + '-' + monthName + '-' + i, 'YYYY-MMMM-D').format('YYYY-MM-DD');
     var dayOfTheWeek = moment(currentDate, "YYYY-MM-DD").format('ddd');
-    console.log('dayoftheweek', dayOfTheWeek);
-    console.log('currendata', currentDate);
-    // var currentDayObj = new DayObj(i, dayOfTheWeek, currentDate);
-    console.log(currentDayObj);
-    // printCalendar(currentDayObj);
+    // console.log('dayoftheweek', dayOfTheWeek);
+    // console.log('currendata', currentDate);
     printCalendar(i, dayOfTheWeek, currentDate);
-    // console.log(i, monthName);
   }
 }
-function DayObj(dayNum, dayOfWeek, currentDate) {
-  this.dayNum = dayNum;
-  this.dayOfWeek = dayOfWeek;
-  this.currentDate = currentDate;
-}
 
-
-  function printCalendar(dayNum, dayOfWeek, currentDate) {
+//stampa calendario a schermo con handlebars
+function printCalendar(dayNum, dayOfWeek, currentDate) {
   var source = document.getElementById('calendar-template').innerHTML;
   var calendarTemplate = Handlebars.compile(source);
-  // var calendarData = { dayNumber: day, monthName: monthName};
   var calendarData = { dayNum: dayNum, dayOfWeek: dayOfWeek, currentDate: currentDate };
   var htmlcalendarData = calendarTemplate(calendarData);
   $('.container.days-container').append(htmlcalendarData);
 }
 
-
+//controlla festività presenti e le associa all'attributo html corrispondente
 function checkFestivity(objMonth) {
   objMonth.forEach(el => {
     var festName = el.name;
     var date = el.date;
     console.log('festname', festName, date);
-    //aggiungo festività in pagina e cambio background cella
+    //aggiungo festività in pagina e cambio bg-color cella
     $(`.date[data-name="${date}"]`)
       .html(festName)
       .closest('.days')
